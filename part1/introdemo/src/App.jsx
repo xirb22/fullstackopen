1b8276417300b31bdb64fca085d3f9344d2d823a
Xirb22 <brix.derouck@hotmail.com>
2025-03-02 19:10:54 +0100

    Completed the introduction to React

--- part1/introdemo/src/App.jsx
+++ part1/introdemo/src/App.jsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const Hello = (props) => {
+  console.log(props)
+  return (
+    <div >
+      <p>Hello {props.name}, you are {props.age} years old!</p>
+    </div>
+  )
+}
+
+
+
+const App = () => {
+  
+  console.log('Hello from component App')
+
+  const now = Date()
+  const a = 10
+  const b = 20
+  console.log(now, a+b)
+
+  const friends = [
+    {name: "Peter", age: 4},
+    {name: "Maya", age: 10}
+  ]
+
+  const friendarray = ["Bob", "Sarah"]
+
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <h1>Friends</h1>
+      {/* <p>{friends[0]}</p>   This throws an error, because friends[0] is an object and values evaluated in the curly braces need to be primitive values (e.g. int, string)*/}
+      <p>{friends[0].name} {friends[0].age}</p>
+      <p>{friends[1].name} {friends[1].age}</p>
+      <p>{friendarray}</p> 
+
+      <h1>Greetings</h1>
+      <Hello name = "George" age = {26 + 10} />
+      <Hello name = "Daisy" age = {12} />
+      <p>{a} + {b} = {a+b}</p>
+    </div>
   )
 }
 
