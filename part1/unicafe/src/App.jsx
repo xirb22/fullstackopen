c4319f7351758b7f6017aba21a5ceed37e4ba1d2
xirb22 <brix.derouck@hotmail.com>
2025-04-05 13:31:44 +0200

    Excercise 1.6 Completed

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -8,7 +8,15 @@ const App = () => {
 
   return (
     <div>
-      code here
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>Good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+      <button onClick={() => setBad(bad + 1)}>Bad</button>
+
+      <h1>Statistics</h1>
+      <p>Good: {good}</p>
+      <p>Neutral: {neutral}</p>
+      <p>Bad: {bad}</p>
     </div>
   )
 }
